e8663b36cb28d9c58f5deb952c42f05a253e3f53
nmhaaa3218 <manhha.nguyen@student.adelaide.edu.au>
2024-05-12 13:01:10 +0930

    create env

---
+++ main.py
@@ -0,0 +1,8 @@
+# Author: Manh Ha Nguyen - Finance and Data Science Undergraduate at the University of Adelaide
+# Date: 2021-09-26
+# Description: main file for deploying the model on streamlit
+
+import streamlit as st
+import pandas as pd
+
+print("Hello World!")
